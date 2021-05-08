50737d4a04e5d65180fc51c3d6d5696a8a8bf8c8
marm0nt <81480193+marm0nt@users.noreply.github.com>
2021-05-07 20:25:26 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CalculatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int answer = firstNumber + secondNumber;
+
+            Console.WriteLine("{0} + {1} = {2}", firstNumber, + secondNumber, answer);
+        }
+
+    }
+}
+
